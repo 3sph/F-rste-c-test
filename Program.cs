c06e8085d1886a5b1642f32647ffce38c1ed015b
3sph <141910502+3sph@users.noreply.github.com>
2023-11-24 15:50:03 +0100

    fikset

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("heisann! hva heter du?");
+        var input = Console.ReadLine();
+        Console.WriteLine($"heisann: {input}");
+    }
+}
